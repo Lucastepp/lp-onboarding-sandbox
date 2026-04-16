ce28f6632a6854d3f2f918c5cbd24e0cd4cbe4a5
Lucastepp <lucastep.uk@gmail.com>
2026-04-16 23:47:52 +0100

    Create API onboarding repository and interface

---
+++ LP.Api.Customer.Onboarding/Repositories/IOnboardingRepository.cs
@@ -0,0 +1,9 @@
+﻿using LP.Api.Customer.Onboarding.Entities;
+
+namespace LP.Api.Customer.Onboarding.Repositories;
+
+public interface IOnboardingRepository
+{
+    void Save(OnboardingEntity onboarding);
+    OnboardingEntity? GetById(Guid id);
+}
---
+++ LP.Api.Customer.Onboarding/Repositories/OnboardingRepository.cs
@@ -0,0 +1,18 @@
+﻿using LP.Api.Customer.Onboarding.Entities;
+
+namespace LP.Api.Customer.Onboarding.Repositories;
+
+public class OnboardingRepository : IOnboardingRepository
+{
+    private static readonly List<OnboardingEntity> _storage = new();
+
+    public void Save(OnboardingEntity onboarding)
+    {
+        _storage.Add(onboarding);
+    }
+
+    public OnboardingEntity? GetById(Guid id)
+    {
+        return _storage.FirstOrDefault(x => x.Id == id);
+    }
+}
