8a88da26d7f9f84473fd06ba9498fc2277bed2c3
Lucastepp <lucastep.uk@gmail.com>
2026-04-16 23:55:33 +0100

    Register services on Program.cs

--- LP.Api.Customer.Onboarding/Program.cs
+++ LP.Api.Customer.Onboarding/Program.cs
@@ -1,3 +1,6 @@
+using LP.Api.Customer.Onboarding.Repositories;
+using LP.Api.Customer.Onboarding.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IOnboardingRepository, OnboardingRepository>();
+builder.Services.AddScoped<IOnboardingService, OnboardingService>();
 
 var app = builder.Build();
 
