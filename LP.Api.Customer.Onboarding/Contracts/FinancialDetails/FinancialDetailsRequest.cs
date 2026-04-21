2a3727f55ab429ab3dec0b42d5c2994cbcc6eebf
Lucastepp <lucastep.uk@gmail.com>
2026-04-21 20:34:08 +0100

    Create Financial details contract and entity

---
+++ LP.Api.Customer.Onboarding/Contracts/FinancialDetails/FinancialDetailsRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace LP.Api.Customer.Onboarding.Contracts.FinancialDetails
+{
+    public class FinancialDetailsRequest
+    {
+        // How data is collected
+        public bool UseOpenBanking { get; set; }
+        public bool HasUploadedDocuments { get; set; }
+
+        // Manual input
+        public decimal? AnnualRevenue { get; set; }
+        public decimal? MonthlyRevenue { get; set; }
+        public decimal? MonthlyExpenses { get; set; }
+
+    }
+}
---
+++ LP.Api.Customer.Onboarding/Entities/FinancialDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace LP.Api.Customer.Onboarding.Entities
+{
+    public class FinancialDetails
+    {
+        public bool UseOpenBanking { get; set; }
+        public bool HasUploadedDocuments { get; set; }
+
+        public decimal? AnnualRevenue { get; set; }
+        public decimal? MonthlyRevenue { get; set; }
+        public decimal? MonthlyExpenses { get; set; }
+    }
+}
