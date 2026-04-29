b8a2bc748c5d002bd8957a44d14c8ed3164d9502
Lucastepp <lucastep.uk@gmail.com>
2026-04-29 22:53:49 +0100

    Create first filter

---
+++ LP.Api.Customer.Onboarding/Filters/LoggingActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LP.Api.Customer.Onboarding.Filters;
+
+public class LoggingActionFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        Console.WriteLine($"[START] Executing {context.ActionDescriptor.DisplayName}");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        Console.WriteLine($"[END] Executed {context.ActionDescriptor.DisplayName}");
+    }
+}
